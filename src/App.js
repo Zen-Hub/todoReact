8867d9fbfcdc6d4d0bb9fa27b823d4b0c34bc7e6
Sergey <inboxshow1@gmail.com>
2023-02-20 23:58:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from 'react';
 import './App.css';
 
+import ToDoitem from './ToDO/ToDoitem'
+import TodoData from './ToDO/TodoData'
+
 function App() {
+
+  const todoItems = TodoData.map(itemArr => {
+    return (
+      <ToDoitem
+        key={itemArr._id}
+        inpTextProps={itemArr.inpText}
+      />
+    )
+  })
+
   return (
     <div className="App">
-      ToDo
+      {todoItems}
+
     </div>
   );
 }
