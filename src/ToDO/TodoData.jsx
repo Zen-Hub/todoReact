4958a1a33b9f149b244e1dfeff216cc01a2e1bdc
Sergey <inboxshow1@gmail.com>
2023-02-26 13:07:56 +0300

    Update TodoData.jsx

--- src/ToDO/TodoData.jsx
+++ src/ToDO/TodoData.jsx
@@ -3,22 +3,22 @@ import "./index.css";
 
 const todoDateArr = [
   {
-    _id: Date.now(),
+    _id: 0,
     inpText: "GO BALI",
     isChecked: true,
   },
   {
-    _id: Date.now(),
+    _id: 1,
     inpText: "TAKE MANY",
     isChecked: false,
   },
   {
-    _id: Date.now(),
+    _id: 2,
     inpText: "свалить из страны",
     isChecked: false,
   },
   {
-    _id: Date.now(),
+    _id: 3,
     inpText: "прожить на острове",
     isChecked: false,
   },
