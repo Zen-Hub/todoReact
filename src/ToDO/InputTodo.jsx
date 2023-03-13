b34a27023d5f835819d4145c3beb9b49d49fa725
Sergey <inboxshow1@gmail.com>
2023-03-13 23:57:53 +0300

    Create InputTodo.jsx

---
+++ src/ToDO/InputTodo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./index.css";
+
+const Input = (props) => {
+    return(
+
+
+        <div>
+
+
+           <input className="inpTask" type="text" />
+
+           <button className="btnstyle">DONE</button>
+           
+        </div>
+    )
+
+    
+}
