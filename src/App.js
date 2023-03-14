508242afb62a96924bd8a969184c47e5b10cf8e9
Sergey <inboxshow1@gmail.com>
2023-03-14 23:56:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,32 @@ import React from 'react';
 import './App.css';
 import ToDoItemFun from './ToDO/ToDoitem'
 import todoDateArr from './ToDO/TodoData'
+import Input from './ToDO/InputTodo'
 
 function App() {
 
+  
+
   const doneAddFun = (id) => {
-    console.log('DONe',id)
+    console.log('DONE',id)
   }
+
+  
 
   const todoItems = todoDateArr.map(itemArr => {
     return (
       <ToDoItemFun
         key={todoDateArr._id}
         inpTextProps={todoDateArr.inpText}
-        addDoneProps={() => { doneAddFun(todoDateArr._id) }}
+        funAddDoneProps={(id) => { doneAddFun(todoDateArr._id) }}
       />
     )
   })
 
   return (
     <div className="App">
+
+      
       {todoItems}
 
     </div>
@@ -28,4 +35,3 @@ function App() {
 }
 
 export default App;
-
