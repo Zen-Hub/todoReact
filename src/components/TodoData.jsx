5979190a8b72105479005ee60023370e1b160e92
Sergey <inboxshow1@gmail.com>
2023-03-20 23:56:45 +0300

    Add files via upload

---
+++ src/components/TodoData.jsx
@@ -0,0 +1,27 @@
+import "./index.css";
+
+
+const todoDateArr = [
+  {
+    _id: 0,
+    inpText: "GO BALI",
+    isChecked: true,
+  },
+  {
+    _id: 1,
+    inpText: "TAKE MANY",
+    isChecked: false,
+  },
+  {
+    _id: 2,
+    inpText: "свалить из страны",
+    isChecked: false,
+  },
+  {
+    _id: 3,
+    inpText: "прожить на острове",
+    isChecked: false,
+  },
+];
+
+export default todoDateArr;
